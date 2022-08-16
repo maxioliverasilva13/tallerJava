c929dc1bd4d8e18359d74cc9607ce4cab1862156
Maximiliano Olivera Silva <maximiliano.olivera@dualbootpartners.com>
2022-08-16 12:48:03 -0300

    other commit

---
+++ src/mygym/logica/modelos/UsuarioModelo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mygym.logica.modelos;
+
+/**
+ *
+ * @author maximilianooliverasilva
+ */
+public class UsuarioModelo {
+    
+}
